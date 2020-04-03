949a2636bf3be26d9454c928f3d7780ebcdb7714
Yan Takushevich <ytakushevich@lift.co>
2020-04-03 13:31:05 +0300

    Migrate to postgres.

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,4 @@ new GraphQLServer({
   schema,
   context: createContext,
   middlewares: [permissions],
-}).start(() =>
-  console.log(
-    `🚀 Server ready at: http://localhost:4000\n⭐️ See sample queries: http://pris.ly/e/ts/graphql-auth#using-the-graphql-api`,
-  ),
-)
+}).start(() => console.log(`🚀 Server ready at: http://localhost:4000`))
